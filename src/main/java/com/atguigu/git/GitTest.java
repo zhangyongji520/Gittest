9cb6555055ec71cafe89a5a565505880f8497f51
zhangyongji520 <70025697+zhangyongji520@users.noreply.github.com>
2022-05-25 23:26:07 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,6 @@ public class GitTest {
         System.out.println("pull test");
          System.out.println("啊哈哈 test");
         System.out.println("这是B");
-        System.out.println("这是A");
+        System.out.println("这是A git更新");
     }
 }
